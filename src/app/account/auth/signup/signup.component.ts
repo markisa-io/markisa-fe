2fef2a9b2d0c5800f192add70eb1504a5224181e
ERachmadZ <38646549+ERachmadZ@users.noreply.github.com>
2021-02-14 22:43:22 +0700

    MAR-21 - one page signup

--- src/app/account/auth/signup/signup.component.ts
+++ src/app/account/auth/signup/signup.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { SignUpState } from '../../../shared/common';
 
 @Component({
     selector: 'app-signup',
@@ -11,16 +10,18 @@ import { SignUpState } from '../../../shared/common';
 export class SignupComponent implements OnInit, AfterViewInit {
 
     signupForm!: FormGroup;
+    submitted = false;
     error = '';
-    state: SignUpState = SignUpState.FirstPage;
 
     constructor(private formBuilder: FormBuilder) { }
 
     ngOnInit() {
         this.signupForm = this.formBuilder.group({
-            name: ['', Validators.required],
             email: ['', [Validators.required, Validators.email]],
-            password: ['', Validators.required],
+            fullname: ['', Validators.required],
+            companyname: ['', Validators.required],
+            //phone: ['', Validators.pattern('/^(^\+62\s?|^0)(\d{3,4}-?){2}\d{3,4}$/g')],
+            password: ['', Validators.required]
         });
     }
 
@@ -29,24 +30,13 @@ export class SignupComponent implements OnInit, AfterViewInit {
         document.body.classList.add('authentication-bg-pattern');
     }
 
-
     // convenience getter for easy access to form fields
     get f() { return this.signupForm.controls; }
 
-    displayFirstPage(): boolean {
-        return this.state == SignUpState.FirstPage;
-    }
-
-    displaySecondPage(): boolean {
-        return this.state == SignUpState.SecondPage;
-    }
-
     onSubmit() {
-        if (this.state = SignUpState.FirstPage)
-            this.state = SignUpState.SecondPage;
-        else
-            this.sendSignUpRequest();
-            //this.state = SignUpState.FirstPage
+        this.submitted = true;
+
+        console.log("submit");
     }
 
     sendSignUpRequest() {
--- src/app/shared/common.ts
+++
@@ -1,4 +0,0 @@
-export enum SignUpState {
-    FirstPage,
-    SecondPage
-}
